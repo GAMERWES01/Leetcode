60ddfcb1b680c9696455d6571d0ab97445646b9a
GAMERWES01 <148088336+GAMERWES01@users.noreply.github.com>
2025-05-13 15:52:41 +0200

    vraag 9 en 66 gedaan esy w

---
+++ Leetcode/1-9999/1-999/1-99/1-9/_9_Palindrome_Number.cs
@@ -0,0 +1,24 @@
+﻿namespace Leetcode._1_9999._1_999._1_99._1_9;
+
+public class _9_Palindrome_Number
+{
+    public bool IsPalindrome(int x) {
+
+        if (x < 0)
+        {
+            return false;
+        }
+        string s = x.ToString();
+
+        int check = s.Length / 2;
+
+        for (int i = 0; i < check; i++)
+        {
+            if (s[i] != s[s.Length-1-i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Leetcode/1-9999/1-999/1-99/60-69/_66_Plus_One.cs
@@ -0,0 +1,23 @@
+﻿namespace Leetcode._1_9999._1_999._1_99._60_69;
+
+public class _66_Plus_One {
+    public int[] PlusOne(int[] digits)
+    {
+        for (int i = digits.Length-1; i >= 0; i--)
+        {
+            if (digits[i] < 9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            else
+            {
+                digits[i] = 0;
+            }
+        }
+        int[] nieuw = new int[digits.Length + 1];
+        nieuw[0] = 1;
+        Array.Copy(digits, 0, nieuw, 1, digits.Length);
+        return nieuw;
+    }
+}

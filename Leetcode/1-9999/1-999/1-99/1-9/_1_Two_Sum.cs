5abeec70a72b7061e0d037209b27b6fd46342ee8
GAMERWES01 <148088336+GAMERWES01@users.noreply.github.com>
2025-06-11 10:20:31 +0200

    Create _1_Two_Sum.cs

---
+++ Leetcode/1-9999/1-999/1-99/1-9/_1_Two_Sum.cs
@@ -0,0 +1,24 @@
+﻿namespace Leetcode._1_9999._1_999._1_99._1_9;
+
+public class _1_Two_Sum
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> dic = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (dic.ContainsKey(target - nums[i]))
+            {
+                return new int[]
+                {
+                    i, dic[target - nums[i]]
+                };
+            }
+            else if (!dic.ContainsKey(nums[i]))
+            {
+                dic.Add(nums[i], i);
+            }
+        }
+        return new int[1];
+    }
+}
